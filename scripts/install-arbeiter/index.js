10bd47ec10c27a75e0ec7485d351337354573208
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-11-08 17:31:40 +0100

    first steps toward multi node setup

---
+++ scripts/install-arbeiter/index.js
@@ -0,0 +1,6 @@
+import fs from "fs/promises";
+import path from "path";
+import * as l from "/pektin-compose/scripts/common/lib.js";
+import { config } from "dotenv";
+config();
+const dir = "/pektin-compose/";
