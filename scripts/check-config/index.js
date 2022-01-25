1dd73dfc6f94b799cff4fa68255ec82fe1d42bb3
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-01-25 15:52:30 +0100

    rename to uninstall

--- scripts/check-config/index.js
+++ scripts/check-config/index.js
@@ -1,3 +1,6 @@
 import { checkConfig } from "@pektin/config";
 
-await checkConfig("/pektin-compose/pektin-config.json", "node_modules/@pektin/config/schema.yml");
+await checkConfig(
+    "/pektin-compose/pektin-config.json",
+    "node_modules/@pektin/config/pektin-config.schema.yml"
+);
--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # remove old containers and volumes in development environment
 if [[ $1 = "delete-old" ]]
 then
-sh delete.sh
+sh uninstall.sh
 fi
 
-# clean up pektin-install
-docker rm pektin-compose-check-config -v
+# clean up pektin-config
+docker rm pektin-compose-check-config -v &>  /dev/null
 
 # check config
 docker run --env UID=$(id -u) --env GID=$(id -g) --name pektin-compose-check-config --mount "type=bind,source=$PWD,dst=/pektin-compose/" -it $(docker build -q ./scripts/check-config/ --no-cache) || exit 1
 
-# clean up pektin-install
-docker rm pektin-compose-check-config -v
+# clean up pektin-config
+docker rm pektin-compose-check-config -v &>  /dev/null
 
 # start vault
 docker-compose -f pektin-compose/pektin.yml up -d vault
