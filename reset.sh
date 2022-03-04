b494c850c6a2dd3f72f1dd82b316cd7b0d0ad4bb
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-03-04 22:10:47 +0100

    add reset without env file

--- reset.sh
+++ reset.sh
@@ -2,6 +2,7 @@
 echo "Deleting old artifacts..."
 docker swarm leave --force
 docker-compose --env-file secrets/.env -f pektin-compose/pektin.yml down --remove-orphans
+docker-compose -f pektin-compose/pektin.yml down --remove-orphans
 docker rm pektin-vault --force -v
 docker volume rm pektin-compose_vault pektin-compose_db
 rm -rf your-infos.md update.sh start.sh stop.sh secrets/ arbeiter/ swarm.sh
