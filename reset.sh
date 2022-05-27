4320a77f8d494b21621be9009a7f2ac9f27cf7fd
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-05-27 17:06:10 +0200

    add grafana jaeger and prom config

--- reset.sh
+++ reset.sh
@@ -3,6 +3,6 @@ echo "Deleting old artifacts..."
 docker swarm leave --force
 docker compose -f pektin-compose/pektin.yml down --remove-orphans
 docker rm pektin-vault --force -v
-docker volume rm pektin-compose_vault pektin-compose_db
+docker volume rm pektin-compose_vault pektin-compose_db pektin-compose_grafana pektin-compose_prometheus
 rm -rf your-infos.md update.sh start.sh stop.sh secrets/ arbeiter/ swarm.sh
 docker image rm pektin-compose-check-config pektin-compose-install pektin-compose-first-start pektin-compose-start --force
